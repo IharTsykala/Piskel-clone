9c7ae662504f9e6891c57b82d9dcbb2ffda87ab6
IharTsykala <sudja09@bk.ru>
2019-07-08 02:40:04 +0300

    feat: add gh-pages

---
+++ precache-manifest.e065865941ed386a7393fddc9b5ad59e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777ca16a1dd6ff208c54f00a54d88b90",
+    "url": "/piskel-clone/index.html"
+  },
+  {
+    "revision": "de26f5384a7d107cb60e",
+    "url": "/piskel-clone/static/css/main.88ac5ea2.chunk.css"
+  },
+  {
+    "revision": "1f9a5f07a807c0f98b87",
+    "url": "/piskel-clone/static/js/2.908bc86b.chunk.js"
+  },
+  {
+    "revision": "de26f5384a7d107cb60e",
+    "url": "/piskel-clone/static/js/main.33e983f5.chunk.js"
+  },
+  {
+    "revision": "5654ab04200be8a1ca6b",
+    "url": "/piskel-clone/static/js/runtime~main.dff1cf59.js"
+  }
+]);
